f887fb12f58258bdfc3abd26359508b963587a47
ml.chien242@gmail.com <ml.chien242@gmail.com>
2024-09-10 16:00:53 +0000

    created intermediate table joining staging:product table on staging:sales table

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,11 @@
+SELECT
+  date_date
+  , sales.orders_id
+  , sales.product_id
+  , revenue
+  , quantity
+  , (sales.quantity - product.purchase_price) AS purchase_cost
+  , revenue - (sales.quantity - product.purchase_price) AS margin
+FROM {{ ref("stg_gz_raw_data__sales")}} AS sales
+INNER JOIN {{ ref("stg_gz_raw_data__product")}} AS product
+  ON sales.product_id = product.products_id;
